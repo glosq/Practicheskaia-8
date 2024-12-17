ee021dad774d9a0149028f1d4de492a969d01162
исп-34 <-34@LAB44-11>
2024-12-17 12:19:27 +0300

    Добавьте файлы проекта.

---
+++ Practicheskaia 8/Class.cs
@@ -0,0 +1,48 @@
+﻿namespace Practicheskaia_8
+{
+    interface Figura
+    {
+        public int Perimetr { get; set; }
+        public int Plochad { get; set; }
+    }
+    interface IComparable
+    {
+        bool CompareTo(object obj);
+    }
+    class Pramoygolnik : Figura, IComparable
+    {
+        public int Perimetr { get; set; }
+        public int Plochad { get; set; }
+        public Pramoygolnik(int perimetr, int plochad)
+        {
+            Perimetr = perimetr;
+            Plochad = plochad;
+        }
+        public string Information()
+        {
+            string info = "Периметр прямоугольника:" + Perimetr + "Площадь прямоугольника:" + Plochad;
+            return info;
+        }
+        public bool CompareTo(object obj)
+        {
+            Kryg kryg = (Kryg)obj;
+            if (kryg.Plochad == this.Plochad) return true;
+            return false;
+        }
+    }
+    class Kryg : Figura
+    {
+        public int Perimetr { get; set; }
+        public int Plochad { get; set; }
+        public Kryg(int perimetr, int plochad)
+        {
+            Perimetr = perimetr;
+            Plochad = plochad;
+        }
+        public string Information()
+        {
+            string info = "Периметр круга: " + Perimetr + " Площадь круга: " + Plochad;
+            return info;
+        }
+    }
+}
---
+++ Practicheskaia 8/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace Practicheskaia_8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int.TryParse(tbPerimetrKryga.Text, out int perimetrKrega);
+            int.TryParse(tbPloshadKryga.Text, out int ploshadKryga);
+            int.TryParse(tbPerimetrPramoygolnika.Text, out int perinmetrPramoegolnika);
+            int.TryParse(tbPloshadPramoygolnika.Text, out int ploshadPramoygolnika);
+            Pramoygolnik pramoygolnik = new Pramoygolnik(perinmetrPramoegolnika, ploshadPramoygolnika);
+            Kryg kryg = new Kryg(perimetrKrega, ploshadKryga);
+            string resultSravnenia = "";
+            string infoPramoygolnika = pramoygolnik.Information();
+            string infoKryga = kryg.Information();
+            if (pramoygolnik.CompareTo(kryg) == true) resultSravnenia = "Пложади равны";
+            else resultSravnenia = "Площади не равны";
+            tbResult.Text = infoPramoygolnika + "\n" + infoKryga + "\n" + resultSravnenia;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа");
+        }
+    }
+}
